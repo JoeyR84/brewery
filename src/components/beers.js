d5cc78a23d9c318bd091efcfd4f42cb0f28020be
Joseph Ricketts <josephricketts@spires-MacBook-Pro-2.local>
2019-02-20 16:18:33 -0700

    initial commit

---
+++ src/components/beers.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class beers extends Component {
+  render() {
+    return <div />;
+  }
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,15 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import { Global, css } from "@emotion/core";
+import styled from "@emotion/styled";
+
+const Wrapper = styled("div")`
+  border: 2px solid green;
+  padding: 10px;
+`;
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -17,30 +23,36 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <Wrapper>
+        <Global
+          styles={css`
+            div {
+            }
+          `}
+        />
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            paddingTop: 0
           }}
         >
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
+            © {new Date().getFullYear()}, I am
             {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <a href="https://www.github.com/joeyr84">Joey</a>
           </footer>
         </div>
-      </>
+      </Wrapper>
     )}
   />
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
